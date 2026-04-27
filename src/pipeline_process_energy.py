3e77a685d21728f72923295f4d986617d1bcfb98
zapata <david.zapata@uni-paderborn.de>
2026-04-27 19:59:55 +0200

    pipeline ready to run in nohup

--- src/pipeline_process_energy.py
+++ src/pipeline_process_energy.py
@@ -1783,9 +1783,9 @@ process_datasets_to_model_sensors['process_4'] = process_datasets_to_model_senso
 # process_datasets_to_model_sensors['process_4']['sensors_to_model'] = ['temp_nach_WR2_(WT2)_5s_energy']
 
 process_datasets_to_model_sensors['process_2'] = process_datasets_to_model_sensors.get('process_2', {})
-process_datasets_to_model_sensors['process_2']['objects_to_model'] = ['l01']
-process_datasets_to_model_sensors['process_2']['activities_to_model'] = ['Produktion']
-process_datasets_to_model_sensors['process_2']['sensors_to_model'] = ['pro_volstrom_l/h_energy']
+# process_datasets_to_model_sensors['process_2']['objects_to_model'] = ['l01']
+# process_datasets_to_model_sensors['process_2']['activities_to_model'] = ['Produktion']
+# process_datasets_to_model_sensors['process_2']['sensors_to_model'] = ['pro_volstrom_l/h_energy']
 
 process_datasets_to_model_sensors['process_3'] = process_datasets_to_model_sensors.get('process_3', {})
 # process_datasets_to_model_sensors['process_3']['objects_to_model'] = ['tower_1']
@@ -1798,7 +1798,7 @@ process_datasets_to_model_sensors['process_3'] = process_datasets_to_model_senso
 # %%
 
 processes_to_run = ['process_2', 'process_3', 'process_4']
-processes_to_run = ['process_2']
+# processes_to_run = ['process_2']
 
 
 
